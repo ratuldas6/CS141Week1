d1d6da513181164461be610c266c6e686b04226e
root <root@LAPTOP-C6O32V4E.localdomain>
2019-08-08 19:12:58 +0530

    done ww test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS141Week1
 Week1 code
+q1.sh works fine
+q2.sh doesn't work properly. Can't figure out how to name multiple files inside folder.
---
+++ q2.sh
@@ -0,0 +1,12 @@
+read -p "Enter your name: " name
+mkdir $name
+name1 = "$(name)1"
+name2 = "$(name)2"
+name3 = "$(name)3"
+name4 = "$(name)4"
+name5 = "$(name)5"
+touch $name/$name1.cpp
+touch $name/$name2.cpp
+touch $name/$name3.cpp
+touch $name/$name4.cpp
+touch $name/$name5.cpp
